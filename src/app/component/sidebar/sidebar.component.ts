4550f4488d46c0991a2518cb49c1aa36c476bdc4
Vinit Shukla <vshukla@argusoft.com>
2023-09-14 15:44:30 +0530

    Icon added and provided router link

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isSidebarExpanded: boolean = true;
 
+  toggleSidebar() {
+    this.isSidebarExpanded = !this.isSidebarExpanded;
+  }
 }
